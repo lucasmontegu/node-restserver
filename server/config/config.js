ed7d542851ca8c9cd18ccd54598808424ee82990
lucas Montegu <lmontegu@gmail.com>
2019-02-11 22:17:03 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -5,3 +5,22 @@
 //========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//========================
+//      Entorno
+//========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//========================
+//      Entorno
+//========================
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:Carajo+36925@ds131905.mlab.com:31905/cafe'
+}
+
+process.env.URLDB = urlDB;
